c3d4004783a9be3dd962e587f9768099cc95fc7e
abdullahal-baki <abdullahalbaki009@gmail.com>
2023-10-12 22:29:50 +0600

    complete desktop version

--- script.js
+++ script.js
@@ -4,11 +4,6 @@ const cc = document.getElementById('content');
 
 const dv = document.getElementById('demo-div');
 
-console.log('hi');
-
-cc.addEventListener('scroll', show());
-
-dv.onscroll = show2();
 
 function show(){
      console.log('hello');
@@ -20,4 +15,50 @@ function show(){
 
 function show2(){
      dv.style.transform = 'translateY(-30px)';
+}
+
+// menu items functions
+const breakfastBtn = document.getElementById("breakfast");
+const lunchBtn = document.getElementById("lunch");
+const dinnerBtn = document.getElementById("dinner");
+
+const breakfastItem = document.getElementById("breakfast-items");
+const lunchItem = document.getElementById("lunch-items");
+const dinnerItem = document.getElementById("dinner-items");
+
+breakfastBtn.addEventListener('click',ShowBreakfastItems);
+function ShowBreakfastItems() {
+     lunchBtn.classList.remove('active');
+     dinnerBtn.classList.remove('active');
+     lunchItem.style.display = "none";
+     dinnerItem.style.display = "none";
+     
+     breakfastBtn.classList.add('active');
+     breakfastItem.style.display = "block";
+     console.log('showing breakfast')
+}
+
+lunchBtn.addEventListener('click',ShowLunchItems);
+function ShowLunchItems() {
+     breakfastBtn.classList.remove('active');
+     dinnerBtn.classList.remove("active");
+     breakfastItem.style.display = "none";
+     dinnerItem.style.display = "none";
+     
+     lunchBtn.classList.add('active');
+     lunchItem.style.display = "block";
+     console.log('showing lunch')
+}
+
+
+dinnerBtn.addEventListener('click',ShowDinnerItems);
+function ShowDinnerItems() {
+     breakfastBtn.classList.remove('active');
+     lunchBtn.classList.remove("active");
+     breakfastItem.style.display = "none";
+     lunchItem.style.display = "none";
+     
+     dinnerBtn.classList.add('active');
+     dinnerItem.style.display = "block";
+     console.log('showing dinner')
 }
